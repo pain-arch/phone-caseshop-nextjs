3b57066c8a28b994946cda591a0b2a6257cea13c
pain-arch <himulol123@gmail.com>
2024-08-08 22:28:42 +0600

    feat: Add Steps component for displaying multi-step process in layout

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWithWrapper from "@/components/MaxWithWrapper";
+import Steps from "@/components/Steps";
 import { ReactNode } from "react";
 
 const Layout = ({ children } : {children: ReactNode}) => {
     return (
         <MaxWithWrapper className="flex-1 flex flex-col">
+            <Steps />
             {children}
        </MaxWithWrapper>
     )
