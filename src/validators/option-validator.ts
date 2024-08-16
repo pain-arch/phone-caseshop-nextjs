b4d0cf0643e7c015b284c7cb303fee2ef47dc9a5
pain-arch <himulol123@gmail.com>
2024-08-16 14:02:10 +0600

    feat: Add option-validator.ts file with COLORS constant array

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,5 @@
+export const COLORS = [
+    { label: 'Black', value: 'black', tw: 'zinc-900' },
+    { label: 'blue', value: 'blue', tw: 'blue-950' },
+    { label: 'rose', value: 'rose', tw: 'rose-950' },
+] as const;
