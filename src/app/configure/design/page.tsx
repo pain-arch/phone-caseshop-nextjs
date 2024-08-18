f5bd5d1e85724a39d344d97cd8836432c6e2016b
pain-arch <himulol123@gmail.com>
2024-08-18 20:59:15 +0600

    feat: Add page component for design configuration

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,32 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    }
+}
+
+const page = async ({ searchParams }: PageProps) => {
+    const { id } = searchParams;
+
+    if (!id || typeof id !== "string") {
+        return notFound();
+    }
+
+    const configuration = await db.configuration.findUnique({
+        where: { id: id }
+    })
+    if (!configuration) {
+        return notFound();
+    }
+
+    const { imageUrl, width, height } = configuration;
+
+
+    return <DesignConfigurator configId={configuration.id} imageUrl={imageUrl} imageDimensions={{ width, height }} />;
+
+}
+
+export default page;
